42e17903d2f87f4f8d46c5ecbca81bb78a879048
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-10-26 21:47:53 +0300

    New BASE_URL for tests added

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -6,7 +6,7 @@ import os
 fake = Faker()
 
 # Flask microservice Base URL
-BASE_URL = os.getenv("BASE_URL") # insead of "DATABASE_URL"
+BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:5000/api/v1/customers") # insead of "DATABASE_URL"
 
 
 # Fixture for customer data creation
