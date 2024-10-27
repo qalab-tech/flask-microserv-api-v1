3f3c8e503592dce1e58e7ee4005b8e32f51681cd
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-10-27 17:16:16 +0300

    New db.py for Debug

--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ from app.logger_config import setup_logger
 
 logger = setup_logger("db_connection")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://127.0.0.1:5432/postgres")
 
 # Init Pool
 try:
