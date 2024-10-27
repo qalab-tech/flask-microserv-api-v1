51dcb85aa8997b4bc1b8ccc15dc92e53a206741c
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-10-27 19:22:12 +0300

    New DATABASE_URL for db

--- app/db.py
+++ app/db.py
@@ -6,7 +6,8 @@ from app.logger_config import setup_logger
 logger = setup_logger("db_connection")
 
 # DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_URL = "postgresql://127.0.0.1:5432/postgres?user=postgres&password=Gl00m88"
+#DATABASE_URL = "postgresql://:5432/postgres?user=postgres&password=Gl00m88"
+BATABASE_URL = "postgresql://postgres:Gl00m88@192.168.88.18/postgres"
 # Init Pool
 try:
     connection_pool = psycopg2.pool.SimpleConnectionPool(1, 20, DATABASE_URL)
