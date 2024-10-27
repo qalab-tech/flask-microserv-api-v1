14ecfd51ab8c07c46d68592a3a7142414c162375
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-10-27 17:38:11 +0300

    DATABASE_URL added to db.py for Debug

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -6,7 +6,8 @@ import os
 fake = Faker()
 
 # Flask microservice Base URL
-BASE_URL = os.getenv("BASE_URL")
+# BASE_URL = os.getenv("BASE_URL")
+BASE_URL = "http://127.0.0.1:5000/api/v1/customers"
 
 
 # Fixture for customer data creation
