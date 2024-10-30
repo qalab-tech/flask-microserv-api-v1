f23e82ee39e4e0f15d95f5019064230f09216a70
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-10-30 17:42:01 +0300

    JWT Auth support added

--- app/controllers/customer_controller.py
+++ app/controllers/customer_controller.py
@@ -2,9 +2,30 @@ from flask import Blueprint, jsonify, request
 from app.services.customer_service import get_customers, get_customer_by_id, create_customer, update_customer, \
     delete_customer
 from app.logger_config import setup_logger
+import jwt
+import os
+
 
 logger = setup_logger("customer_controller")
 customer_bp = Blueprint('customers', __name__)
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get("Authorization")
+        if not token:
+            return jsonify({"message": "Token is missing!"}), 403
+        try:
+            # Token veryfication
+            jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return jsonify({"message": "Token expired!"}), 403
+        except jwt.InvalidTokenError:
+            return jsonify({"message": "Invalid token!"}), 403
+        return f(*args, **kwargs)
+    return decorated
 
 
 @customer_bp.route('/', methods=['GET'])
@@ -23,6 +44,7 @@ def create_customer_route():
 
 
 @customer_bp.route('/<int:customer_id>', methods=['GET'])
+@token_required
 def get_customer_route(customer_id):
     """GET customer by id"""
     customer = get_customer_by_id(customer_id)
