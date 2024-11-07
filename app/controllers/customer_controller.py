97cc3aaaad1be09f71b562792998145a5dfceb95
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-11-07 21:22:12 +0300

    abort token errors handling added

--- app/controllers/customer_controller.py
+++ app/controllers/customer_controller.py
@@ -34,16 +34,16 @@ def token_required(f):
     def decorated(*args, **kwargs):
         token = request.headers.get("Authorization")
         if not token:
-            return jsonify({"message": "Token is missing!"}), 403
+            abort(403, "Token is missing!")
 
         try:
             # Devide key for "Bearer" and key itself
             token = token.split()[1] if "Bearer" in token else token
             jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
         except jwt.ExpiredSignatureError:
-            return jsonify({"message": "Token has expired"}), 401
+            abort(401, "Token has expired")
         except jwt.InvalidTokenError:
-            return jsonify({"message": "Invalid token"}), 403
+            abort(403, "Invalid token")
 
         return f(*args, **kwargs)
 
