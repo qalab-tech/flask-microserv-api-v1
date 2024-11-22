fe14e299330013ea7fe8215f12e3ab1e87ce5c2b
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-11-22 17:30:48 +0300

    New tests/test_customers.py

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -1,46 +1,15 @@
 import requests
 import pytest
-from faker.proxy import Faker
 from tests.utils.decorators import handle_requests_exceptions
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# fake = Faker()
 
 # Flask microservice Base URL
 
 BASE_URL = os.getenv("CUSTOMERS_BASE_URL")
 AUTH_BASE_URL = os.getenv("AUTH_BASE_URL")
-
-
-# @pytest.fixture(scope="session")
-# def auth_token():
-#     """Get auth token"""
-#     url = f"{AUTH_BASE_URL}/auth/login"  # authorization URL
-#     credentials = {"username": "test", "password": "test"}
-#     response = requests.post(url, json=credentials)
-#     response.raise_for_status()
-#     token = response.json()["token"]
-#     return f"Bearer {token}"
-#
-#
-# @pytest.fixture
-# def new_customer_data():
-#     """Fake customer data generation"""
-#     return {"name": fake.name(), "address": fake.address()}
-#
-#
-# @pytest.fixture
-# def new_customer(new_customer_data, auth_token):
-#     """Create a new customer in the database and delete after the test"""
-#     headers = {"Authorization": auth_token}
-#     response = requests.post(BASE_URL, json=new_customer_data, headers=headers)
-#     assert response.status_code == 201
-#     customer = response.json()
-#     yield customer
-#     # Delete customer after test
-#     requests.delete(f"{BASE_URL}/{customer['customer_id']}", headers=headers)
 
 
 @handle_requests_exceptions
