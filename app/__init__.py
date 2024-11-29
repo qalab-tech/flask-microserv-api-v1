65b3f3e940e338908563df3e1910f718aa014712
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-11-30 01:06:25 +0300

    Customers service added to Prometheus monitoring

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from app.controllers.customer_controller import customers_bp
-
+from prometheus_flask_exporter import PrometheusMetrics
 
 def create_app():
     app = Flask(__name__)
     app.url_map.strict_slashes = False
-
+    PrometheusMetrics(app)
     # Register BluePrint
     app.register_blueprint(customers_bp, url_prefix='/api/v1')
 
